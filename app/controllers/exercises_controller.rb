ebac43f92d939fd995867367100b1ebfebd9c66c
fgolfc <cf.ts.fujimoto@gmail.com>
2023-01-15 18:34:58 +0900

    2time

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,14 +2,14 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food.left_outer_joins(:order_foods).where.not('order_foods.food_id = foods.id')
+    @foods = Food.left_outer_joins(:order_foods).where(order_foods: {food_id: nil})
 
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop.left_outer_joins(:foods).where.not(orders: :order_foods)
+    @shops = Shop.left_outer_joins(:foods).where(shops: {shop_id: nil})
   end
 
   def exercise3 
